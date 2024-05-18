8f2827df9db94c8770d45fd79667209d97e3926a
Julien Caucheteux <jdaodkql@gmail.com>
2024-05-18 09:37:49 +0200

    init: setup project

---
+++ SwiftyProteins/SwiftyProteinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyProteinsApp.swift
+//  SwiftyProteins
+//
+//  Created by Julien Caucheteux on 18/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyProteinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
