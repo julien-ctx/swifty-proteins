13d6148894f6aad25d3b8f0135f430fe80650ff5
Julien Caucheteux <jdaodkql@gmail.com>
2024-05-30 16:01:12 +0200

    feat: position camera according to protein size

--- SwiftyProteins/Views/ProteinView/MoleculeView.swift
+++ SwiftyProteins/Views/ProteinView/MoleculeView.swift
@@ -25,7 +25,7 @@ struct MoleculeView: UIViewRepresentable {
     func updateUIView(_ uiView: SCNView, context: Context) {}
     
     private func createScene() -> SCNScene {
-        let scene = SCNScene()
+        var scene = SCNScene()
         
         for atom in molecule.atoms {
             let sphere = SCNSphere(radius: 0.3)
@@ -35,12 +35,45 @@ struct MoleculeView: UIViewRepresentable {
             scene.rootNode.addChildNode(node)
         }
         
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 10)
-        scene.rootNode.addChildNode(cameraNode)
+        addCamera(for: &scene)
         
         return scene
+    }
+    
+    private func degreesToRadians(_ number: Double) -> Double {
+        return number * .pi / 180
+    }
+    
+    // https://stackoverflow.com/questions/21544336/how-to-position-the-camera-so-that-my-main-object-is-entirely-visible-and-fit-to
+    // https://forum.unity.com/threads/fit-object-exactly-into-perspective-cameras-field-of-view-focus-the-object.496472/
+    private func addCamera(for scene: inout SCNScene) -> Void {
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        
+        let minAtomX = molecule.atoms.min { $0.x < $1.x }
+        let maxAtomX = molecule.atoms.max { $0.x < $1.x }
+        
+        let minAtomY = molecule.atoms.min { $0.y < $1.y }
+        let maxAtomY = molecule.atoms.max { $0.y < $1.y }
+        
+        let minAtomZ = molecule.atoms.min { $0.z < $1.z }
+        let maxAtomZ = molecule.atoms.max { $0.z < $1.z }
+        
+        if let minX = minAtomX?.x, let maxX = maxAtomX?.x, let minZ = minAtomZ?.z, let maxZ = maxAtomZ?.z, let minY = minAtomY?.y, let maxY = maxAtomY?.y, let fov = cameraNode.camera?.fieldOfView {
+            
+            let cameraDistance = 2.0;
+            let proteinSize = [maxX - minX, maxY - minY, maxZ - minZ].max()
+            let cameraView = 2.0 * tan(0.5 * degreesToRadians(fov))
+            
+            guard let maxSize = proteinSize else {
+                return
+            }
+            var distance = cameraDistance * maxSize / cameraView
+            distance += 0.5 * maxSize;
+            
+            cameraNode.position = SCNVector3(x: 0, y: 0, z: Float(distance))
+            scene.rootNode.addChildNode(cameraNode)
+        }
     }
     
     // https://en.wikipedia.org/wiki/CPK_coloring
